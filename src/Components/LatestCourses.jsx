da8fea97771b5eee074ca6a52cb8e842fa7b96f0
developer77 <hoseinbakhshalizadeh+github@gmail.com>
2024-04-14 20:37:42 +0330

    courseBox comp added and styled

--- src/Components/LatestCourses.jsx
+++ src/Components/LatestCourses.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import SectionHeader from './SectionHeader'
+import CourseBox from './CourseBox'
 export default function LatestCourses() {
     return (
         <div class="courses">
-            <SectionHeader title="جدیدترین دوره ها" desc="سکوی پرتاپ شما به سمت موفقیت" btnTitle="تمامی دوره ها"/>
             <div class="container">
-
+                <SectionHeader title="جدیدترین دوره ها" desc="سکوی پرتاپ شما به سمت موفقیت" btnTitle="تمامی دوره ها" />
+                <div className='course-wrapper mt-20 grid grid-cols-4 gap-10'>
+                    <CourseBox/>
+                    <CourseBox/>
+                    <CourseBox/>
+                    <CourseBox/>
+                    <CourseBox/>
+                    <CourseBox/>
+                </div>
             </div>
         </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,29 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  ],darkMode:"class",
   theme: {
+    screens: {
+      'xs': '480px',
+      'sm': '640px',
+
+      'md': '768px',
+
+      'lg': '1024px',
+
+      'xl': '1280px',
+
+
+    },
     extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          lg: "0.625rem"
+        }
+
+      },
       colors:{
         "green-sabzlearn":"#2bce56",
         "dark-sabzlearn":"#464749",
