35a54fa44bef02b38f16b4d72c891a7d854b305f
developer77 <hoseinbakhshalizadeh+github@gmail.com>
2024-04-14 18:35:13 +0330

    lastest comp and sectionHeader comp added

---
+++ src/Components/LatestCourses.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SectionHeader from './SectionHeader'
+export default function LatestCourses() {
+    return (
+        <div class="courses">
+            <SectionHeader title="جدیدترین دوره ها" desc="سکوی پرتاپ شما به سمت موفقیت" btnTitle="تمامی دوره ها"/>
+            <div class="container">
+
+            </div>
+        </div>
+
+
+    )
+}
---
+++ src/Components/SectionHeader.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function SectionHeader({ title, desc, btnTitle }) {
+    return (
+        <div class="courses-header">
+            <div class="courses-header__right">
+                <span class="courses-header__title title">{title}</span>
+                <span class="courses-header__text">{desc}</span>
+            </div>
+            {btnTitle && <div class="courses-header__left">
+                <a href="#" class="courses-header__link">
+                    {btnTitle}
+                    <i class="fas fa-arrow-left courses-header__icon"></i>
+                </a>
+            </div>}
+        </div>
+    )
+}
