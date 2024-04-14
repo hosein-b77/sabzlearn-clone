9aef1cd0f2360ba8dec6a0b9e03678c6058b9189
developer77 <hoseinbakhshalizadeh+github@gmail.com>
2024-04-14 23:12:15 +0330

    Landing count comp developed for count dynamiclly in landing section

---
+++ src/Components/LandingCount.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react'
+
+export default function LandingCount({ count }) {
+    const [counter, setCounter] = useState(0)
+    useEffect(() => {
+        let interval = setInterval(() => {
+            setCounter(preval => preval + 1)
+        }, 1)
+        if (counter === count) {
+            clearInterval(interval)
+        }
+        return () => clearInterval(interval) //this line is for cleanUp in React and prevent memory leak because in every render,new interval created and old one still remained 
+    }, [counter])
+    return (
+        <span class="landing-status__count">{counter}</span>
+    )
+}
