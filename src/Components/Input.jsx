41ae79106ffc153ef949e74e6613cb01b49758cb
developer77 <hoseinbakhshalizadeh+github@gmail.com>
2024-05-22 23:05:30 +0330

    reducer developed in input comp

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 
+const inputReducer = (state, action) => {
+    switch (action.type) {
+        case 'CHANGE': {
+            return {
+                ...state,
+                value: action.value,
+                isValid: action.isValid
+            }
+        }
+        default: {
+            return state
+        }
+    }
+}
 export default function Input(props) {
+    const [mainInput, dispatch] = useReducer(inputReducer, {
+        value: '',
+        isValid: false
+    })
     const onChangeHandler = (event) => {
         console.log(event.target.value)
+        dispatch({ type: 'CHANGE', value: event.target.value, isValid: true })
     }
     const element = props.element === 'input' ? (
-        <input onChange={onChangeHandler} className={props.className} type={props.type} placeholder={props.placeholder} />
+        <input value={mainInput.value} onChange={onChangeHandler} className={`${props.className} ${mainInput.isValid ? 'border bottom-2 !border-green-sabzlearn' : 'border bottom-2 !border-red-600' }`} type={props.type} placeholder={props.placeholder} />
     ) : (
-        <textarea onChange={onChangeHandler} className={props.className} placeholder={props.placeholder} />
+        <textarea value={mainInput.value} onChange={onChangeHandler} className={props.className} placeholder={props.placeholder} />
     );
 
     return (
