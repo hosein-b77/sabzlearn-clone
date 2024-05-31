480d36a39461c61309f0972a3dd741853581c459
developer77 <hoseinbakhshalizadeh+github@gmail.com>
2024-05-31 21:52:08 +0330

    popular courses  DEVELOPED by swiper

--- src/Components/PopularCourses.jsx
+++ src/Components/PopularCourses.jsx
@@ -1,14 +1,54 @@
-import React from 'react'
-import SectionHeader from './SectionHeader'
-export default function PopularCourses() {
+import React, { useState, useEffect } from 'react';
+import SectionHeader from './SectionHeader';
+import CourseBox from './CourseBox';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/autoplay';
+import { Navigation, Autoplay } from 'swiper/modules';
+
+export default function PreSellCourses() {
+    const [popular, setPopular] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:4000/v1/courses/popular")
+            .then(res => res.json())
+            .then(data => setPopular(data));
+    }, []);
+
     return (
-        <div className="popular">
+        <div className="presell">
             <div className="container">
-                <SectionHeader title="محبوب ترین دوره ها" desc=" " />
-        
+                <SectionHeader
+                    title="دوره های محبوب"
+                    desc="متن تستی برای توضیحات دوره های محبوب"
+                />
+
+                <div className="courses-content">
+                    <div className="container">
+                        <div className="row px-4">
+                            <Swiper
+                                slidesPerView={3}
+                                spaceBetween={30}
+                                loop={true}
+                                navigation={true}
+                                autoplay={{
+                                    delay: 3000,
+                                    disableOnInteraction: false
+                                }}
+                                modules={[Navigation, Autoplay]}
+                                className="mySwiper"
+                            >
+                                {popular.map((course) => (
+                                    <SwiperSlide key={course._id}>
+                                        <CourseBox {...course} />
+                                    </SwiperSlide>
+                                ))}
+                            </Swiper>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    )
-    }
-   
-              
+    );
+}
