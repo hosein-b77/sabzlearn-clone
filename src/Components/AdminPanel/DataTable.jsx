e78297f79b7b11ceb80d303e27700dbbd4c0ede3
developer77 <hoseinbakhshalizadeh+github@gmail.com>
2024-06-04 00:52:28 +0330

    DataTable changed  and course list now shown with Api in admin panel

--- src/Components/AdminPanel/DataTable.jsx
+++ src/Components/AdminPanel/DataTable.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const DataTable = ({ title, data, deleteUser, banUser }) => {
+const DataTable = ({ title, headers, children }) => {
 
     return (
         <div className="container mx-auto p-4">
@@ -9,39 +9,21 @@ const DataTable = ({ title, data, deleteUser, banUser }) => {
                 <thead>
                     <tr>
                         <th className="py-2 px-4 border-b">R</th>
-                        <th className="py-2 px-4 border-b">نام و خانوادگی</th>
-                        <th className="py-2 px-4 border-b">شماره</th>
-                        <th className="py-2 px-4 border-b">ایمیل</th>
-                        <th className="py-2 px-4 border-b">ویرایش</th>
-                        <th className="py-2 px-4 border-b">حذف</th>
-                        <th className="py-2 px-4 border-b">بن</th>
+                        {
+                            headers.length>0 &&(
+                                headers.map((header,index)=>{
+                                    return(
+                                        <th key={index} className="py-2 px-4 border-b">{header}</th>
+                                    )
+                                })
+                            )
+                        }
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        data.length > 0 && (
-                            data.filter(user => user.role !== 'ADMIN').map((user, index) => {
-                                return (
-                                    <tr key={user._id} className='text-center'>
-                                        <td className="py-2 px-4 border-b">{index + 1}</td>
-                                        <td className="py-2 px-4 border-b">{user.name}</td>
-                                        <td className="py-2 px-4 border-b">{user.phone === "" ? 'ثبت نشده' : user.phone}</td>
-                                        <td className="py-2 px-4 border-b">{user.email}</td>
-                                        <td className="py-2 px-4 border-b">
-                                            <button className="bg-blue-500 text-white py-1 px-2 rounded">ویرایش</button>
-                                        </td>
-                                        <td className="py-2 px-4 border-b">
-                                            <button onClick={() => deleteUser(user._id)} className="bg-red-500 text-white py-1 px-2 rounded">حذف</button>
-                                        </td>
-                                        <td className="py-2 px-4 border-b">
-                                            <button onClick={() => banUser(user._id)} className="bg-red-500 text-white py-1 px-2 rounded">بن</button>
-                                        </td>
-                                    </tr>
-                                )
-                            })
-                        )
+                        children
                     }
-
                 </tbody>
             </table>
         </div>
--- src/Pages/AdminPanel/Courses.jsx
+++ src/Pages/AdminPanel/Courses.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DataTable from '../../Components/AdminPanel/DataTable'
 
 export default function Courses() {
+  const [allCourses, setAllCourses] = useState([])
+  useEffect(() => {
+    getUsers()
+  }, [])
+  function getUsers() {
+    fetch('http://localhost:4000/v1/courses')
+      .then(res => res.json())
+      .then(data => { console.log(data); setAllCourses(data) })
+  }
   return (
-    <div>Courses</div>
+    <>
+      <DataTable title="کاربران" headers={['عنوان', 'مبلغ', 'وضعیت', 'لینک', 'مدرس', 'دسته بندی', 'ویرایش', 'حذف']}>
+        {
+          allCourses.length > 0 && (
+            allCourses.map((course, index) => {
+              return (
+                <tr key={course._id} className='text-center'>
+                  <td className="py-2 px-4 border-b">{index + 1}</td>
+                  <td className="py-2 px-4 border-b">{course.name}</td>
+                  <td className="py-2 px-4 border-b">{course.price === 0 ? 'رایگان' : course.price.toLocaleString()}</td>
+                  <td className="py-2 px-4 border-b">{course.isComplete === 0 ? 'در حال برگزاری' : 'به اتمام رسیده'}</td>
+                  <td className="py-2 px-4 border-b">{course.shortName}</td>
+                  <td className="py-2 px-4 border-b">{course.creator}</td>
+                  <td className="py-2 px-4 border-b">{course.categoryID.title}</td>
+                  <td className="py-2 px-4 border-b">
+                    <button className="bg-blue-500 text-white py-1 px-2 rounded">ویرایش</button>
+                  </td>
+                  <td className="py-2 px-4 border-b">
+                    <button className="bg-red-500 text-white py-1 px-2 rounded">حذف</button>
+                  </td>
+
+                </tr>
+              )
+            })
+          )
+        }
+      </DataTable>
+
+    </>
   )
 }
