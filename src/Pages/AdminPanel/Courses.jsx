89dd58b61ea1b9113fc0de30f3b43336fd943b5b
developer77 <hoseinbakhshalizadeh+github@gmail.com>
2024-06-04 23:58:12 +0330

    delete feature developed in couse page in admin page

--- src/Pages/AdminPanel/Courses.jsx
+++ src/Pages/AdminPanel/Courses.jsx
@@ -1,19 +1,49 @@
 import React, { useEffect, useState } from 'react'
 import DataTable from '../../Components/AdminPanel/DataTable'
-
+import swal from 'sweetalert'
 export default function Courses() {
   const [allCourses, setAllCourses] = useState([])
   useEffect(() => {
-    getUsers()
+    getAllCourses()
   }, [])
-  function getUsers() {
+  function getAllCourses() {
     fetch('http://localhost:4000/v1/courses')
       .then(res => res.json())
       .then(data => { console.log(data); setAllCourses(data) })
   }
+  const deleteCategory = (courseID) => {
+    swal({
+      title: "ایا از حذف این دوره مطمعن هستید؟",
+      icon: 'warning',
+      buttons: ['خیر', 'بلی']
+    }).then(result => {
+      if (result) {
+        const localStorageData = JSON.parse(localStorage.getItem("user"));
+        fetch(`http://localhost:4000/v1/courses/${courseID}`, {
+          method: 'DELETE',
+          headers: {
+            Authorization: `Bearer ${localStorageData.token}`,
+          }
+        })
+          .then(res => {
+            if (res.ok) {
+              swal({
+                title: "با موفقیت حدف گردید",
+                icon: 'success',
+                buttons: 'ok',
+                className: "text-center"
+              }).then(() => {
+                getAllCourses()
+              })
+            }
+          })
+
+      }
+    })
+  }
   return (
     <>
-      <DataTable title="کاربران" headers={['عنوان', 'مبلغ', 'وضعیت', 'لینک', 'مدرس', 'دسته بندی', 'ویرایش', 'حذف']}>
+      <DataTable title="دوره" headers={['عنوان', 'مبلغ', 'وضعیت', 'لینک', 'مدرس', 'دسته بندی', 'ویرایش', 'حذف']}>
         {
           allCourses.length > 0 && (
             allCourses.map((course, index) => {
@@ -30,7 +60,7 @@ export default function Courses() {
                     <button className="bg-blue-500 text-white py-1 px-2 rounded">ویرایش</button>
                   </td>
                   <td className="py-2 px-4 border-b">
-                    <button className="bg-red-500 text-white py-1 px-2 rounded">حذف</button>
+                    <button onClick={() => deleteCategory(course._id)} className="bg-red-500 text-white py-1 px-2 rounded">حذف</button>
                   </td>
 
                 </tr>
