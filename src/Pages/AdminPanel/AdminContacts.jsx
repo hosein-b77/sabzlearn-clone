d67c71f916ffcef4ed20d08d0574c3bd221112c7
developer77 <hoseinbakhshalizadeh+github@gmail.com>
2024-06-05 16:18:29 +0330

    users contacts awnser to user by email  developed in admni panel

--- src/Pages/AdminPanel/AdminContacts.jsx
+++ src/Pages/AdminPanel/AdminContacts.jsx
@@ -20,9 +20,38 @@ export default function AdminContacts() {
         
         })
     }
+    const sendAnwserToUser = (contactEmail) => {
+        const localStorageData = JSON.parse(localStorage.getItem('user'))
+        swal({
+            title: "متن پاسخ را وارد کنید",
+            content: 'input',
+            buttons: "ارسال ایمیل"
+        }).then(value => {
+            console.log(value);
+
+            const anwserInfo = {
+                email: contactEmail,
+                answer: value
+            }
+
+            fetch('http://localhost:4000/v1/contact/answer', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    "Authorization": `Bearer ${localStorageData.token}`
+                },
+                body: JSON.stringify(anwserInfo)
+            }).then(res => {
+                console.log(res);
+                if (res.ok) {
+                    return res.json()
+                }
+            }).then(result => console.log(result))
+        })
+    }
     return (
         <>
-            <DataTable title="کاربران" headers={['نام و نام خانوادگی', 'شماره تماس', 'ایمیل', 'مشاهده', 'حذف', 'پاسخ']}>
+            <DataTable title="کاربران" headers={['نام و نام خانوادگی', 'شماره تماس', 'ایمیل', 'مشاهده', 'پاسخ', 'حذف']}>
 
                 {allContacts.length > 0 && (allContacts.map((contact, index) => {
                     return (
@@ -35,10 +64,10 @@ export default function AdminContacts() {
                                 <button onClick={() => seeMsg(contact.body)} className="bg-blue-500 text-white py-1 px-2 rounded">مشاهده</button>
                             </td>
                             <td className="py-2 px-4 border-b">
-                                <button className="bg-red-500 text-white py-1 px-2 rounded">حذف</button>
+                                <button onClick={()=>sendAnwserToUser(contact.email)} className="bg-blue-500 text-white py-1 px-2 rounded">پاسخ</button>
                             </td>
                             <td className="py-2 px-4 border-b">
-                                <button className="bg-blue-500 text-white py-1 px-2 rounded">پاسخ</button>
+                                <button className="bg-red-500 text-white py-1 px-2 rounded">حذف</button>
                             </td>
                         </tr>
                     )
