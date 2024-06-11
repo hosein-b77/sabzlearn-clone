9b60832be31f08c7eac2846f5451a7d03ce7c766
developer77 <hoseinbakhshalizadeh+github@gmail.com>
2024-06-11 18:19:25 +0330

    admin panel route protection fix bug

--- src/Components/PAdminPrivate/PAdminPrivate.jsx
+++ src/Components/PAdminPrivate/PAdminPrivate.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AuthContext from '../../context/authContext';
 
@@ -6,17 +6,11 @@ export default function PAdminPrivate({ children }) {
     const authContext = useContext(AuthContext);
     const navigate = useNavigate();
 
-    useEffect(() => {
-        if (!authContext.isLoggedIn) {
-            navigate("/login");
-        } else if (authContext.userInfos.role !== 'ADMIN') {
-            navigate("/");
-        }
-    }, [authContext, navigate]);
-
-    if (!authContext.isLoggedIn || authContext.userInfos.role !== 'ADMIN') {
-        return null; // Or you can return a loading indicator or another component
-    }
-
-    return <>{children}</>;
+    return (
+        <>
+            {
+                authContext.userInfos.role === 'ADMIN' ? <>{children}</> : navigate('/login')
+            }
+        </>
+    )
 }
